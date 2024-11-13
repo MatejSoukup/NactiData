8a2f3ce5a93d2ff438919c0debaf9ecd85220acf
MatejSoukup <90925007+MatejSoukup@users.noreply.github.com>
2024-11-13 14:49:14 +0100

    Add project files.

---
+++ NactiData/Data.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NactiData
+{
+    public class Data
+    {
+        public double Number1 { get; private set; }
+        public double Number2 { get; private set; }
+        public double Number3 { get; private set; }
+
+        public Data()
+        {
+            // Načtení tří hodnot z konzole
+            Console.Write("Zadejte první číslo: ");
+            Number1 = ReadConsoleDouble();
+
+            Console.Write("Zadejte druhé číslo: ");
+            Number2 = ReadConsoleDouble();
+
+            Console.Write("Zadejte třetí číslo: ");
+            Number3 = ReadConsoleDouble();
+
+            Console.WriteLine(Add());
+            Console.WriteLine(Product());
+            Console.WriteLine(Operation());
+
+        }
+
+        public double Add()
+        {
+            return Number1 + Number2 + Number3; 
+        }
+
+        public double Product()
+        {
+            return Number1 * Number2 * Number3;
+        }
+
+        public double Operation()
+        {
+            if (Number3 != 0)
+            {
+                return (Number1 + Number2) / Number3;
+            }
+            WriteError("Dělení nulou");
+            return double.NaN;
+        }
+
+        private double ReadConsoleDouble()
+        {
+            while (true)
+            {
+                try
+                {
+                    return double.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    WriteError("Neplatný formát čísla.");
+                    Console.WriteLine("Zadejte prosím platné číslo:");
+                }
+            }
+        }
+
+        private void WriteError(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        
+
+
+    }
+}
---
+++ NactiData/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NactiData;
+
+Console.WriteLine("Hello, World!");
+
+Data data = new Data();
+
+Console.ReadLine();
